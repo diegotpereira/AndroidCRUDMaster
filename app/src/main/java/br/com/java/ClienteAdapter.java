4dcee622f47c8e45333aa363110151dcae392e7c
Diegotpereira <diegoteixeirapereira@hotmail.com>
2020-11-10 12:42:11 -0300

    alteração no arquivo classe ClienteAdapter.java adicionado botão editar

--- app/src/main/java/br/com/java/ClienteAdapter.java
+++ app/src/main/java/br/com/java/ClienteAdapter.java
@@ -1,7 +1,13 @@
 package br.com.java;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,6 +25,10 @@ public class ClienteAdapter extends RecyclerView.Adapter<ClienteHolder> {
         clientes.add(cliente);
         notifyItemInserted(getItemCount());
     }
+    public void atualizarCliente(Cliente cliente){
+        clientes.set(clientes.indexOf(cliente), cliente);
+        notifyItemChanged(clientes.indexOf(cliente));
+    }
 
     @Override
     public ClienteHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -29,10 +39,34 @@ public class ClienteAdapter extends RecyclerView.Adapter<ClienteHolder> {
     @Override
     public void onBindViewHolder(ClienteHolder holder, int position) {
         holder.nomeCliente.setText(clientes.get(position).getNome());
+
+        holder.btnEditar.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Activity activity = getActivity(v);
+                Intent intent = activity.getIntent();
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                intent.putExtra("cliente", cliente);
+                activity.finish();
+                activity.startActivity(intent);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return clientes != null ? clientes.size() : 0;
     }
+
+    private Activity getActivity(View view) {
+        Context context = view.getContext();
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (Activity)context;
+            }
+            context = ((ContextWrapper)context).getBaseContext();
+        }
+        return null;
+    }
+
 }
