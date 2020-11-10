a2bde15416edf1574dd8a3366daa7ab0f2a56e34
Diegotpereira <diegoteixeirapereira@hotmail.com>
2020-11-10 08:59:22 -0300

    criação do arquivo classe ClienteDAO.java adicionado metodo listar

--- app/src/main/java/br/com/java/ClienteDAO.java
+++ app/src/main/java/br/com/java/ClienteDAO.java
@@ -2,6 +2,10 @@ package br.com.java;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteDAO {
     private final String TABLE_CLIENTES = "Clientes";
@@ -17,6 +21,20 @@ public class ClienteDAO {
         cv.put("UF", uf);
         cv.put("Vip", vip ? 1 : 0);
         return gw.getDatabase().insert(TABLE_CLIENTES, null, cv) > 0;
+    }
+    public List<Cliente> retornarTodos(){
+        List<Cliente> clientes = new ArrayList<>();
+        Cursor cursor = gw.getDatabase().rawQuery("SELECT * FROM Clientes", null);
+        while(cursor.moveToNext()){
+            int id = cursor.getInt(cursor.getColumnIndex("ID"));
+            String nome = cursor.getString(cursor.getColumnIndex("Nome"));
+            String sexo = cursor.getString(cursor.getColumnIndex("Sexo"));
+            String uf = cursor.getString(cursor.getColumnIndex("UF"));
+            boolean vip = cursor.getInt(cursor.getColumnIndex("Vip")) > 0;
+            clientes.add(new Cliente(id, nome, sexo, uf, vip));
+        }
+        cursor.close();
+        return clientes;
     }
 
 }
