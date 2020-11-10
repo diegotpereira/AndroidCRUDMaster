019a6152ee9f61353e0c5c25aaa5dcecfcd429df
Diegotpereira <diegoteixeirapereira@hotmail.com>
2020-11-10 11:06:44 -0300

    criação do arquivo classe ClienteAdapter.java

---
+++ app/src/main/java/br/com/java/ClienteAdapter.java
@@ -0,0 +1,33 @@
+package br.com.java;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ClienteAdapter extends RecyclerView.Adapter<ClienteHolder> {
+
+    private final List<Cliente> clientes;
+
+    public ClienteAdapter(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    @Override
+    public ClienteHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ClienteHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_lista, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ClienteHolder holder, int position) {
+        holder.nomeCliente.setText(clientes.get(position).getNome());
+    }
+
+    @Override
+    public int getItemCount() {
+        return clientes != null ? clientes.size() : 0;
+    }
+}
