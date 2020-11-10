e4f0bd9be036b4c816c73a346630f8f5ad198727
Diegotpereira <diegoteixeirapereira@hotmail.com>
2020-11-10 08:44:14 -0300

    criação do arquivo classe Cliente.java

---
+++ app/src/main/java/br/com/java/Cliente.java
@@ -0,0 +1,69 @@
+package br.com.java;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable {
+
+    private int id;
+    private String nome;
+    private String sexo;
+    private String uf;
+    private boolean vip;
+
+    public Cliente(int id, String nome, String sexo, String uf, boolean vip){
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.uf = uf;
+        this.vip = vip;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+    @Override
+    public boolean equals(Object o){
+        return this.id == ((Cliente)o).id;
+    }
+
+    @Override
+    public int hashCode(){
+        return this.id;
+    }
+}
